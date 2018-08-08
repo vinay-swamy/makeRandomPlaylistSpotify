2dd3dd8c6dd807614504124bb37bf38b364d46a1
swamyvs <swamyvs@neie10b11jb85b.nih.gov>
2018-08-08 12:23:07 -0400

    automatically creates playlist

--- randomPlaylist.py
+++ randomPlaylist.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import numpy
 import spotipy
 import spotipy.util as util
@@ -10,28 +11,34 @@ redirect_uri='http://localhost:8888/callback/'
 if len(sys.argv) > 2:
     username = sys.argv[1]
     num_songs=int(sys.argv[2])
+    plname=sys.argv[3]
 else:
     sys.exit('Bad Arguments')
 
 if num_songs>100 or num_songs<1:
     sys.exit('The number of songs must be between 1 and 100')
-# get and check token
-print('Your browser will probably say the site can be reached, just copy and paste the url into the terminal. '  )
+if not os.path.exists('.cache-' + username):
+    print('A webpage will be launched, it will probably say not found, just copy and paste the link into the terminal'  )
 token = util.prompt_for_user_token(username, scope,client_id,client_secret,redirect_uri)
 if token:
     sp = spotipy.Spotify(auth=token)
 else:
-    sys.exit('Bad token')    
-    
- 
+    sys.exit('Bad token')
+
+
 track_ids=[]
 
-playlists = sp.user_playlists(username)
-first=playlists['items'][0]
-targ_playlist=first['uri']
+sp.user_playlist_create(username,plname,public=True,description='made with makeRandomPlaylist')
+playlists = sp.user_playlists(username)['items']
+id=''
+for i in range(len(playlists)):
+    if playlists[i]['name']==plname:
+        id=playlists[i]['uri']
+if not id:
+    sys.exit('unable to find uri for playlist')
 
 
-# get user saved tracks, spotify api only lets you get 50 songs per query, so have to repeateadly query
+#get user saved tracks, spotify api only lets you get 50 songs per query, so have to repeateadly query
 print('getting your saved tracks(this may take a few minutes)')
 os=0
 while os<9999:
@@ -39,8 +46,8 @@ while os<9999:
     for item in results['items']:
         track = item['track']
         track_ids.append(track['uri'])
-   
-    os=os+50   
+
+    os=os+50
     prog='\r' + str(format(os/10000*100,'.2f'))+'%'
     #prog=str(format(os/10000*100))
     sys.stdout.write( prog )
@@ -54,10 +61,6 @@ for i in rand:
 if token:
     sp = spotipy.Spotify(auth=token)
     sp.trace = False
-    sp.user_playlist_add_tracks(username, targ_playlist, out_tracks)
+    sp.user_playlist_add_tracks(username, id, out_tracks)
 
-print('\n',num_songs, ' songs added to ', first['name'])
-
-
-    
-
+print('\n',num_songs, ' songs added to ', plname)
